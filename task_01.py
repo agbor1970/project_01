c610d85b1a03ce2983790ec6f95fa2f356d77b9f
agbor1970 <borodyukh.ag@ya.ru>
2022-11-05 16:11:12 +0300

    added task_01.py

---
+++ task_01.py
@@ -0,0 +1,11 @@
+# Есть строка с перечислением песен
+
+my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, 
+Start Me Up, New Salvation'
+
+# Выведите на консоль с помощью индексации строки, последовательно: первый 
+трек, последний, второй, второй с конца
+# Нельзя переопределять my_favorite_songs и запятая не должна выводиться.
+
+my_list = list(my_favorite_songs.split(', '))
+print(my_list[0], my_list[-1], my_list[1], my_list[-2])
