6232acb555951e02046cf7d0de92b66075214434
Nikita Pyshkov <85805562+pyshkovni@users.noreply.github.com>
2022-11-12 15:20:15 +0300

    Добавил решение

--- homeworks/task_01.py
+++ homeworks/task_01.py
@@ -9,3 +9,36 @@ Start Me Up, New Salvation'
 
 my_list = list(my_favorite_songs.split(', '))
 print(my_list[0], my_list[-1], my_list[1], my_list[-2])
+
+# # Да, ваше решение рабочее, но мы могли бы его улучшить. 
+# Например, не вручную посчитать индекс строки, а попробовать найти индекс через метод для строк find и rfind
+
+# Решение с помощью индекции строк
+
+first_song = my_favorite_songs [
+    : my_favorite_songs.find(',')
+]
+
+last_song = my_favorite_songs [
+    my_favorite_songs.rfind('A') :
+]
+
+second_song = my_favorite_songs[
+     my_favorite_songs.find('N') : 
+     my_favorite_songs.find(', S')
+]
+
+previous_song = my_favorite_songs [
+    my_favorite_songs.rfind('O') : my_favorite_songs.rfind(',')
+    ]
+
+print(first_song, last_song, second_song, previous_song)
+
+
+# Второй вариант, мы можем воспользоваться методом разделения строк по символам. split.
+# Полученный в результате список проиндексируем по песням
+
+# Решение с помощью метода split() и индексации списков
+songs = my_favorite_songs.split(', ')
+
+print(songs[0], songs[-1], songs[1], songs[-2])
