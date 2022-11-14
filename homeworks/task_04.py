d0bb0835ee68ed7029a54b6d59a273b3e8c431c8
agbor1970 <agbor1970@gmail.com>
2022-11-14 23:04:26 +0300

    task_04 done

--- homeworks/task_04.py
+++ homeworks/task_04.py
@@ -18,3 +18,15 @@ my_favorite_songs = [
     ['In This World', 4.02],
 ]
 
+songs_sum  = 0
+
+for num in range(3):
+    a = random.randint(0, 8)
+    songs_sum += int(my_favorite_songs[a][1] // 1 * 60) + int(round(my_favorite_songs[a][1] % 1 * 100, 0))
+
+celoe = songs_sum // 60
+ostatok = songs_sum % 60
+if ostatok < 10:
+    ostatok = str('0' + str(ostatok))
+strin = str(celoe) + '.' + str(ostatok)
+print('Три песни звучат ' + strin + ' минут')
